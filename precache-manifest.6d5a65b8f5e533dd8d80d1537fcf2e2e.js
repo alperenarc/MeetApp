c432915c459ef8d1fecf24f5fd297d7211249a50
Alperen ARICI <alparicieren@gmail.com>
2019-10-09 13:46:41 +0300

    Updates

---
+++ precache-manifest.6d5a65b8f5e533dd8d80d1537fcf2e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0b5bfe09cb79b4d33b163179faa5cf",
+    "url": "/MeetApp/index.html"
+  },
+  {
+    "revision": "6ccfd29a702e2ee0bbef",
+    "url": "/MeetApp/static/css/main.3b9dc181.chunk.css"
+  },
+  {
+    "revision": "2632d4453a594f97f4ac",
+    "url": "/MeetApp/static/js/2.bc78a34f.chunk.js"
+  },
+  {
+    "revision": "6ccfd29a702e2ee0bbef",
+    "url": "/MeetApp/static/js/main.60040e19.chunk.js"
+  },
+  {
+    "revision": "3f96925a49317c53b0d6",
+    "url": "/MeetApp/static/js/runtime~main.c67cac4c.js"
+  },
+  {
+    "revision": "232811189398ab43aeb5a4f7adb2d688",
+    "url": "/MeetApp/static/media/meet-logo.23281118.png"
+  }
+]);
